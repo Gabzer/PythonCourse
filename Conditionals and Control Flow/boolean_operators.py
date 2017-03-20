49d17ff8f11e61394cce330356da8ff62eef6b7b
Gabzer <gab.zer.nas@gmail.com>
2017-03-20 14:19:52 -0300

    Mais alguns passos

---
+++ Conditionals and Control Flow/boolean_operators.py
@@ -0,0 +1,14 @@
+# Torne-me falso!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # Fizemos esta para voce!
+
+# Torne-me verdadeiro!
+bool_two = (True == False) or 2 == 2
+
+# Torne-me falso!
+bool_three = (10 == 11) and (1 == 1)
+
+# Torne-me verdadeiro!
+bool_four = (1 >= 2) or not("Coiso" != "Coiso")
+
+# Torne-me verdadeiro!
+bool_five = ((1 + 1) == 2) and (-(-(-2)) == -2)
