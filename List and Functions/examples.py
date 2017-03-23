43a4eca9e1e156a41eb293cc3949e1bf59970a3c
Gabzer <gab.zer.nas@gmail.com>
2017-03-23 17:55:11 -0300

    passo seguinte

---
+++ List and Functions/examples.py
@@ -0,0 +1,6 @@
+#####################################
+#Way to Remove at list
+n = [1, 3, 5]
+n.pop(1)        #remove the index = 3
+n.remove(1)     #remove the number '1', index = 0 in this case
+del(n[0])       #remove the index = 0
